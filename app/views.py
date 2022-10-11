bb620475ab4896e71cdde7a0c0c9959283d80570
J0harAR <alfaro.ruiz.johan@gmail.com>
2022-10-10 23:27:54 -0500

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request,'app/home.html')
+def tienda(request):
+    return render(request,'app/tienda.html')
+def contacto(request):
+   return render(request,'app/contacto.html')
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from blog.models import post,Categoria
+
+# Create your views here.
+def blog(request):
+    posts=post.objects.all()
+    return render(request,'blog/blog.html',{"posts":posts})
+def categoria(request,categoria_id):
+    categoria=Categoria.objects.get(id=categoria_id)
+    posts=post.objects.filter(categorias=categoria)
+    return render(request,"blog/categorias.html",{"categoria":categoria,"posts":posts})
