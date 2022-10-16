ebd8674a009ae1a4a9eef23eae2e5761f1b88e32
J0harAR <alfaro.ruiz.johan@gmail.com>
2022-10-16 02:04:55 -0500

    modelos tienda

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,31 @@
+from dataclasses import Field
+from email.policy import default
+from enum import auto
+from operator import mod
+from tabnanny import verbose
+from turtle import update
 from django.db import models
 
 # Create your models here.
+class CategoriaProducto(models.Model):
+    nombre=models.CharField(max_length=50)
+    create=models.DateTimeField(auto_now_add=True)
+    update=models.DateField(auto_now_add=True)
+    class Meta:
+        verbose_name="CategoriaProducto"
+        verbose_name_plural="CategoriasProducto"
+    def __str__(self):
+        return self.nombre
+class Producto(models.Model):
+    nombre=models.CharField(max_length=50)
+    categorias=models.ForeignKey(CategoriaProducto,on_delete=models.CASCADE)
+    imagen=models.ImageField(upload_to='tienda',null=True,blank=True)
+    precio=models.FloatField()
+    disponibilidad=models.BooleanField(default=True)
+    class Meta:
+        verbose_name="Producto"
+        verbose_name_plural="Productos"
+    def __str__(self):
+        return self.nombre
+
+
